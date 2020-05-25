75399dc208ce6e4997f9b0ed5ca064c9e984eb32
markwpg <mark.walters@protectgroup.co>
2020-05-25 15:47:05 +0100

    Adding the admin view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SimpleMultiPageApp.Controllers
             return View();
         }
 
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
